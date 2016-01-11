50cde42ffecf04d7a42bd8c45614cbaa6bb561d3
Gijs Molenaar <gijs@pythonic.nl>
2016-01-11 16:32:51 +0200

    new release to test repo signing

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ scripts = [
 
 setup(
     name="anansi_menu",
-    version="0.10",
+    version="0.11",
     packages=find_packages(),
     install_requires=install_requires,
     scripts=scripts,
