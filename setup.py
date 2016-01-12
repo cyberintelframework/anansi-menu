256f8d8977d939dec3db1a9872ae9f80c3b6066b
Gijs Molenaar <gijs@pythonic.nl>
2016-01-12 12:38:27 +0200

    new release, switch to test repo

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ scripts = [
 
 setup(
     name="anansi_menu",
-    version="0.12",
+    version="0.14",
     packages=find_packages(),
     install_requires=install_requires,
     scripts=scripts,
